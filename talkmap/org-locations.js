e198ea75c7f0345ca157084b5ece6129085f7e18
Pranav Kulkarni <pskul@ucdavis.edu>
2025-02-27 14:22:25 -0800

    update talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,42 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "VEEC Study Day 2022<br />Wageningen, the Netherlands; Wageningen, The Netherlands",
+    51.9685817,
+    5.668298061827958
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "UCDavis Postdoctoral Symposium 2024<br />Davis, California, USA; Davis, California, USA",
+    38.5435526,
+    -121.739005
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "ISVEE 2022<br />Halifax, Canada; Halifax, Canada",
+    44.648618,
+    -63.5859487
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "ISVEE 2024<br />Sydney, Australia; Sydney, Australia",
+    -33.8698439,
+    151.2082848
+  ],
+  [
+    "EEID 2024<br />Stanford, California, USA; Stanford, California, USA",
+    37.427467,
+    -122.1702445
+  ],
+  [
+    "SVEPM 2021<br />Toulouse, France; Toulouse, France",
+    43.6044622,
+    1.4442469
+  ],
+  [
+    "ISESSAH 2022<br />Halifax, Canada; Halifax, Canada",
+    44.648618,
+    -63.5859487
+  ],
+  [
+    "ISESSAH 2023<br />Helsinki, Finland; Helsinki, Finland",
+    60.1674881,
+    24.9427473
   ]
 ];
